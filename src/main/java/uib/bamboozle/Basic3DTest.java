e7e2898aca5942a1a039dd6f9e286ea6d2a2c8de
Eirik Strøm <eirikstrom1@gmail.com>
2018-02-15 15:33:52 +0100

    Basic 3d graphics

--- src/main/java/uib/bamboozle/Basic3DTest.java
+++ src/main/java/uib/bamboozle/Basic3DTest.java
@@ -1,18 +1,58 @@
 package uib.bamboozle;
 
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.VertexAttributes.*;
+import com.badlogic.gdx.graphics.g3d.*;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 
 public class Basic3DTest implements ApplicationListener {
+    PerspectiveCamera cam;
+    Environment environment;
+    ModelBatch modelBatch;
+    Model model;
+    ModelInstance instance;
+
     @Override
     public void create () {
+        environment = new Environment();
+        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
+        environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
+
+        modelBatch = new ModelBatch();
+        cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        cam.position.set(10f, 10f, 10f);
+        cam.lookAt(0,0,0);
+        cam.near = 1f;
+        cam.far = 300f;
+        cam.update();
+
+        ModelBuilder modelBuilder = new ModelBuilder();
+        model = modelBuilder.createBox(5f, 5f, 5f,
+                new Material(ColorAttribute.createDiffuse(Color.GREEN)),
+                Usage.Position | Usage.Normal);
+        instance = new ModelInstance(model);
     }
 
     @Override
     public void render () {
+        Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+
+        modelBatch.begin(cam);
+        modelBatch.render(instance, environment);
+        modelBatch.end();
     }
 
     @Override
     public void dispose () {
+        modelBatch.dispose();
+        model.dispose();
     }
 
     @Override
--- src/main/java/uib/bamboozle/Main.java
+++ src/main/java/uib/bamboozle/Main.java
@@ -7,8 +7,8 @@ public class Main {
    public static void main (String[] arg) {
       LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
       config.title = "GoGoLand";
-      config.width = 800;
-      config.height = 480;
+      config.width = 1280;
+      config.height = 800;
       new LwjglApplication(new Basic3DTest(), config);
    }
 }
